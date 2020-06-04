ff16882d93faa30e70783f30c6f95496a9b2a041
Keahooi Hung <keahung@berkeley.edu>
2020-06-03 19:50:21 -0700

    using env var

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
-const db = require('./config/db')
+const url = process.env.PROD_MONGODB
 const app = express()
 const port = 8000;
 
 app.use(bodyParser.urlencoded({extended: true}))
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(url, (err, database) => {
     if (err) {
         return console.log(err)
     }
